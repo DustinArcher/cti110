811c20ebf262d1bbbefec152e00550066e89f1d3
DustinArcher <archerd4812@student.faytechcc.edu>
2022-11-01 14:07:27 -0400

    123123
    1231231

--- P4HW1.py
+++ P4HW1.py
@@ -6,4 +6,23 @@
 #Ask user for 6 grades for 6 modules
 #Add them to a list
 
-grades = [0,0,0,0,0,0]
+grades = []
+
+for grade in range(6):
+    grade = int(input("Please Input Grades For Modules:"))
+    grades.append(grade)
+
+print("Grades:",grades)
+
+print("The Highest Grade was:", max(grades))
+
+print("The Lowest Grade was:", min(grades))
+
+total = sum(grades)
+count = len(grades)
+
+average = total / count
+
+print("Total is:",total)
+
+print("Average is:",average)
